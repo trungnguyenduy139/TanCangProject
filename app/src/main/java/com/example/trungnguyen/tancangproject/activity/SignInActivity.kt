b9364fd48085a24848c1ab1c35d5745310a29e3e
Trung Nguyen <trungnguyenduy139@gmail.com>
2017-10-21 23:47:32 +0700

    Add some package

---
+++ app/src/main/java/com/example/trungnguyen/tancangproject/activity/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.example.trungnguyen.tancangproject.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trungnguyen.tancangproject.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
---
+++ app/src/main/java/com/example/trungnguyen/tancangproject/bean/Driver.kt
@@ -0,0 +1,8 @@
+package com.example.trungnguyen.tancangproject.bean
+
+/**
+ * Author : Trung Nguyen
+ * Date : 10/21/2017
+ */
+class Driver {
+}
